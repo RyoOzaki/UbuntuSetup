d6e076db3f2814362e1bc627a05cff9a8ea9fa99
RyoOzaki <ryo.ozaki@em.ci.ritsumei.ac.jp>
2018-06-14 21:10:12 -0700

    Add goto-definition apm lib.

--- setup2.sh
+++ setup2.sh
@@ -4,7 +4,7 @@ PYTHON_VERSION="3.6.5"
 VIRTUALENV_NAME="dev"
 
 PIP_LIBRARIES="numpy scipy matplotlib scikit-learn tqdm wheel ipython jupyter cython numba joblib autopep8"
-APM_LIBRARIES="autocomplete-python highlight-selected hydrogen"
+APM_LIBRARIES="autocomplete-python highlight-selected goto-definition hydrogen"
 
 ATOM_COFFEE="${HOME}/.atom/init.coffee"
 
