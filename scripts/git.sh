03833fd46775a92518b0de394efd80655ef4282f
RyoOzaki <ryo.ozaki@em.ci.ritsumei.ac.jp>
2019-05-31 21:11:46 -0700

    add: Added rewrite script of import statement of logsumexp

--- scripts/git.sh
+++ scripts/git.sh
@@ -4,7 +4,10 @@ echo "Install Python libraries from git repositories..."
 for GIT_URL in `cat package_lists/git_packages.txt`
 do
   git clone ${GIT_URL} git_work
+  cp scripts/scipy_logsumexp_import_fix.sh git_work/
   cd git_work
+  sh scipy_logsumexp_import_fix.sh
+  rm scipy_logsumexp_import_fix.sh
   python setup.py install
   cd ../
   rm -rf git_work
