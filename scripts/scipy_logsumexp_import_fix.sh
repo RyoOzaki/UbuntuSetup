a4ba8288379b1fc2421e847cae5a4dde91827ceb
RyoOzaki <ryo.ozaki@em.ci.ritsumei.ac.jp>
2019-10-16 20:23:15 -0700

    remove scipy_logsumexp_import_fix because, it fixed in pyhsmm

--- scripts/scipy_logsumexp_import_fix.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-LOGSUMEXP_IMPORT_BEF="from scipy.misc import logsumexp"
-LOGSUMEXP_IMPORT_AFT="try:\n\tfrom scipy.special import logsumexp\nexcept ImportError:\n\tfrom scipy.misc import logsumexp  # removed in scipy 0.19.0"
-
-find . -type f | xargs sed -i "s/${LOGSUMEXP_IMPORT_BEF}/${LOGSUMEXP_IMPORT_AFT}/g"
